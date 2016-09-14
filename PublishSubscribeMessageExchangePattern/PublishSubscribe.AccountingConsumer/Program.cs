3dfa7a46cc5ea97f56fcf3447aa041f3a4dcbea4
Paul <paul.j.millar.1971@gmail.com>
2016-09-14 11:18:03 +0100

    Added Publish / Subscribe Message Exchange Pattern Examples

---
+++ PublishSubscribeMessageExchangePattern/PublishSubscribe.AccountingConsumer/Program.cs
@@ -0,0 +1,64 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQCommon.ConnectionServices;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace PublishSubscribe.AccountingConsumer
+{
+    class Program
+    {
+        private static IConnection connection = ConnectionService.CreateConnection();
+        private static IModel channel = connection.CreateModel();
+
+        static void Main(string[] args)
+        {
+            ReceiveMessagesWithEvents();
+
+            // Give it time to consume messages
+            Thread.Sleep(2000);
+
+            // Close Connection & Channel
+            channel.Close();
+            connection.Close();
+
+            // Finish
+            Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
+            Console.ReadKey();
+        }
+
+        private static void ReceiveMessagesWithEvents()
+        {
+            // Configure how we receive messages
+            channel.BasicQos(0, 1, false); // Process only one message at a time
+
+            // Configure handler for message
+            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(channel);
+            eventingBasicConsumer.Received += OneWayMessageEventReceiver;
+
+            // Consume message from queue
+            channel.BasicConsume("mycompany.queues.accounting", false, eventingBasicConsumer);
+        }
+
+        /// <summary>
+        /// Event Based Message Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OneWayMessageEventReceiver(object sender, BasicDeliverEventArgs e)
+        {
+            IBasicProperties basicProperties = e.BasicProperties;
+            Console.WriteLine("Message received by the event based consumer. Check the debug window for details.");
+            Console.WriteLine($"Message received from the exchange {e.Exchange}");
+            Console.WriteLine($"Content type: {basicProperties.ContentType}");
+            Console.WriteLine($"Consumer tag: {e.ConsumerTag}");
+            Console.WriteLine($"Delivery tag: { e.DeliveryTag}");
+
+            string message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine($"Message: {message}");
+            Console.WriteLine($"Message received by the accounting consumer: { message}");
+            channel.BasicAck(e.DeliveryTag, false);
+        }
+    }
+}
---
+++ PublishSubscribeMessageExchangePattern/PublishSubscribePublisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQCommon.ConnectionServices;
+using System;
+using System.Text;
+
+namespace PublishSubscribePublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Connection and Channel for transporting message
+            IConnection connection = ConnectionService.CreateConnection();
+            IModel channel = connection.CreateModel();
+            Console.WriteLine(string.Concat("Connection open: ", connection.IsOpen));
+
+            // Declare Exchange & Queues
+            channel.ExchangeDeclare("mycompany.fanout.exchange", ExchangeType.Fanout, true, false, null);
+            channel.QueueDeclare("mycompany.queues.accounting", true, false, false, null);
+            channel.QueueDeclare("mycompany.queues.management", true, false, false, null);
+
+            // Bind Queues to Exchange
+            channel.QueueBind("mycompany.queues.accounting", "mycompany.fanout.exchange", "");
+            channel.QueueBind("mycompany.queues.management", "mycompany.fanout.exchange", "");
+
+            // Basic message properties
+            IBasicProperties properties = channel.CreateBasicProperties();
+            properties.Persistent = true; // Keep even after server restart
+            properties.ContentType = "text/plain";
+
+            // Publish a message
+            PublicationAddress address = new PublicationAddress(ExchangeType.Fanout, "mycompany.fanout.exchange", "");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("This is a message from the RabbitMq .NET driver"));
+            Console.WriteLine("Message sent");
+
+            // Close Connection & Channel
+            channel.Close();
+            connection.Close();
+
+            // Finish
+            Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
+            Console.ReadKey();
+        }
+    }
+}
