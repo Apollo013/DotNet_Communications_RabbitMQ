956754bde5630179c72a9cf6d4fa61d6f6d6d2b4
Paul <paul.j.millar.1971@gmail.com>
2016-09-14 09:36:46 +0100

    Added Event Based Consuming

--- OneWayMessageExchangePattern/OneWayConsumer/Program.cs
+++ OneWayMessageExchangePattern/OneWayConsumer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using RabbitMQCommon.ConnectionServices;
 using System;
 using System.Text;
@@ -7,7 +8,15 @@ namespace OneWayConsumer
 {
     class Program
     {
+        private static IModel channelForEventing;
+
         static void Main(string[] args)
+        {
+            // ReceiveMessagesWithDerivedClass ();
+            ReceiveMessagesWithEvents();
+        }
+
+        private static void ReceiveMessagesWithDerivedClass()
         {
             // Create a Connection and Channel for receiving message
             IConnection connection = ConnectionService.CreateConnection();
@@ -30,10 +39,52 @@ namespace OneWayConsumer
             Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
             Console.ReadKey();
         }
+
+        private static void ReceiveMessagesWithEvents()
+        {
+            // Create a Connection and Channel for receiving message
+            IConnection connection = ConnectionService.CreateConnection();
+            channelForEventing = connection.CreateModel();
+
+            // Configure how we receive messages
+            channelForEventing.BasicQos(0, 1, false); // Receive only one message at a time
+
+            // Configure handler for message
+            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(channelForEventing);
+            eventingBasicConsumer.Received += EventingBasicConsumer_Received;
+
+            // Consume message from queue
+            channelForEventing.BasicConsume("my.first.queue", false, eventingBasicConsumer);
+
+            // Close Connection & Channel
+            channelForEventing.Close();
+            connection.Close();
+
+            // Finish
+            Console.WriteLine(string.Concat("Channel is closed: ", channelForEventing.IsClosed));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Event Based Message Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void EventingBasicConsumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            IBasicProperties basicProperties = e.BasicProperties;
+            Console.WriteLine("Message received by the event based consumer. Check the debug window for details.");
+            Console.WriteLine(string.Concat("Message received from the exchange ", e.Exchange));
+            Console.WriteLine(string.Concat("Content type: ", basicProperties.ContentType));
+            Console.WriteLine(string.Concat("Consumer tag: ", e.ConsumerTag));
+            Console.WriteLine(string.Concat("Delivery tag: ", e.DeliveryTag));
+            Console.WriteLine(string.Concat("Message: ", Encoding.UTF8.GetString(e.Body)));
+            channelForEventing.BasicAck(e.DeliveryTag, false);
+        }
     }
 
     /// <summary>
-    /// Message Handler
+    /// Derived Class Message Handler
     /// </summary>
     class OneWayMessageReceiver : DefaultBasicConsumer
     {
