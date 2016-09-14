27b141279753e7901d01dfe9d8b5ad93ec189649
Paul <paul.j.millar.1971@gmail.com>
2016-09-14 13:44:05 +0100

    Added Topics Message Exchange Pattern Examples

---
+++ TopicsMessageExchangePattern/Topics.Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQCommon.ConnectionServices;
+using System;
+using System.Text;
+
+namespace Topics.Consumer
+{
+    class Program
+    {
+        private static IConnection connection = ConnectionService.CreateConnection();
+        private static IModel channel = connection.CreateModel();
+
+        static void Main(string[] args)
+        {
+            // Configure how we receive messages
+            channel.BasicQos(0, 1, false); // Process only one message at a time
+
+            // Configure handler for message
+            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(channel);
+            eventingBasicConsumer.Received += OneWayMessageEventReceiver;
+
+            // Consume message
+            channel.BasicConsume("company.queue.topic", false, eventingBasicConsumer);
+        }
+
+        private static void OneWayMessageEventReceiver(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Message received by the event based consumer. Check the debug window for details.");
+            Console.WriteLine($"Message received from the exchange {e.Exchange}");
+            Console.WriteLine($"Content type: {e.BasicProperties.ContentType}");
+            Console.WriteLine($"Consumer tag: {e.ConsumerTag}");
+            Console.WriteLine($"Delivery tag: {e.DeliveryTag}");
+
+            string message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine($"Message received: {message}");
+
+            // Acknowledge
+            channel.BasicAck(e.DeliveryTag, false);
+        }
+    }
+}
---
+++ TopicsMessageExchangePattern/Topics.Publisher/Program.cs
@@ -0,0 +1,64 @@
+﻿using RabbitMQ.Client;
+using RabbitMQCommon.ConnectionServices;
+using System;
+using System.Text;
+
+namespace Topics.Publisher
+{
+    class Program
+    {
+        private static IConnection connection = ConnectionService.CreateConnection();
+        private static IModel channel = connection.CreateModel();
+
+        static void Main(string[] args)
+        {
+            // Exchange / Queue Declaration & Binding
+            channel.ExchangeDeclare("company.exchange.topic", ExchangeType.Topic, true, false, null);
+            channel.QueueDeclare("company.queue.topic", true, false, false, null);
+
+            // A queue can be dedicated to one or more routing keys.
+            channel.QueueBind("company.queue.topic", "company.exchange.topic", "*.world");
+            channel.QueueBind("company.queue.topic", "company.exchange.topic", "world.#");
+
+            // Basic message properties
+            IBasicProperties properties = channel.CreateBasicProperties();
+            properties.Persistent = true; // Keep even after server restart
+            properties.ContentType = "text/plain";
+
+            // Publish messages (THESE MATCH THE ABOVE PATTERNS)            
+            PublicationAddress address = new PublicationAddress(ExchangeType.Topic, "company.exchange.topic", "beautiful.world");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("The world is beautiful"));
+
+            address = new PublicationAddress(ExchangeType.Topic, "company.exchange.topic", "world.news.and.more");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("It's Friday night"));
+
+            // Publish messages (THESE DO NOT MATCH THE ABOVE PATTERNS !!!)
+            address = new PublicationAddress(ExchangeType.Topic, "company.exchange.topic", "news of the world");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("This is some random news from the world"));
+
+            address = new PublicationAddress(ExchangeType.Topic, "company.exchange.topic", "news.of.the.world");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("trololo"));
+
+            address = new PublicationAddress(ExchangeType.Topic, "company.exchange.topic", "the world is crumbling");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("whatever"));
+
+            address = new PublicationAddress(ExchangeType.Topic, "company.exchange.topic", "the.world.is.crumbling");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("Hello"));
+
+
+            address = new PublicationAddress(ExchangeType.Topic, "company.exchange.topic", "world news and more");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("No more tears"));
+
+
+            Console.WriteLine("Messages sent");
+
+            // Close Connection & Channel
+            channel.Close();
+            connection.Close();
+
+            // Finish
+            Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
+            Console.ReadKey();
+        }
+    }
+}
