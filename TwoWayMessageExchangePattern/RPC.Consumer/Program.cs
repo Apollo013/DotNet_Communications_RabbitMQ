9ce10829291c5bd1bbe6b2417f7a44eaef149895
Paul <paul.j.millar.1971@gmail.com>
2016-09-14 12:16:10 +0100

    Added Two Way MEP with RPC

---
+++ TwoWayMessageExchangePattern/RPC.Consumer/Program.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQCommon.ConnectionServices;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RPC.Consumer
+{
+    class Program
+    {
+        private static IConnection connection = ConnectionService.CreateConnection();
+        private static IModel channel = connection.CreateModel();
+
+        static void Main(string[] args)
+        {
+            // Configure how we receive messages
+            channel.BasicQos(0, 1, false); // Process only one message at a time
+
+            // Configure handler for message
+            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(channel);
+            eventingBasicConsumer.Received += TwoWayMessageEventReceiver;
+
+            // Consume message from queue
+            channel.BasicConsume("mycompany.queues.rpc", false, eventingBasicConsumer);
+
+            // Give it time to process
+            Thread.Sleep(60000);
+
+            // Close Connection & Channel
+            channel.Close();
+            connection.Close();
+
+            // Finish
+            Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
+            Console.ReadKey();
+        }
+
+        private static void TwoWayMessageEventReceiver(object sender, BasicDeliverEventArgs e)
+        {
+            // Decode Message
+            string message = Encoding.UTF8.GetString(e.Body);
+            channel.BasicAck(e.DeliveryTag, false); // acknowledge the message
+
+            // Output message
+            Console.WriteLine($"Message: {message}");
+
+            // Prompt for reply
+            Console.WriteLine("Enter your response: ");
+            string response = Console.ReadLine();
+
+            // Configure reply
+            IBasicProperties replyBasicProperties = channel.CreateBasicProperties();
+            replyBasicProperties.CorrelationId = e.BasicProperties.CorrelationId; // same correlation ID to its message as the one received in the initial message from the publisher
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+            // Send reply
+            channel.BasicPublish("", e.BasicProperties.ReplyTo, replyBasicProperties, responseBytes); // ReplyTo property of the delivery arguments to publish a response to original publisher
+        }
+    }
+}
---
+++ TwoWayMessageExchangePattern/RPC.Publisher/Program.cs
@@ -0,0 +1,90 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQCommon.ConnectionServices;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RPC.Publisher
+{
+    class Program
+    {
+        // Create a Connection and Channel for transporting message
+        private static IConnection connection = ConnectionService.CreateConnection();
+        private static IModel channel = connection.CreateModel();
+        private static string rpcResponseQueue = channel.QueueDeclare().QueueName;
+        private static string correlationId = Guid.NewGuid().ToString();
+        private static string responseFromConsumer = null;
+        private static IBasicProperties basicProperties = channel.CreateBasicProperties();
+        private static string message;
+        private static byte[] messageBytes;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(string.Concat("Connection open: ", connection.IsOpen));
+
+            // Declare a Queue (response queue will be dynamically set up !)
+            channel.QueueDeclare("mycompany.queues.rpc", true, false, false, null);
+
+            // Publisg & Consume messages
+            SendRpcMessagesBackAndForth(channel);
+
+            // Give it time to process
+            Thread.Sleep(60000);
+
+            // Close Connection & Channel
+            channel.Close();
+            connection.Close();
+
+            // Finish
+            Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
+            Console.ReadKey();
+        }
+
+        private static void SendRpcMessagesBackAndForth(IModel channel)
+        {
+            // Configure message
+            basicProperties.ReplyTo = rpcResponseQueue;
+            basicProperties.CorrelationId = correlationId; // message correlation ID so that we can match the sender’s message to the response from the receiver.
+
+            // Prompt for message to publish
+            Console.WriteLine("Enter your message and press Enter.");
+            message = Console.ReadLine();
+            messageBytes = Encoding.UTF8.GetBytes(message);
+
+            // Send It
+            channel.BasicPublish("", "mycompany.queues.rpc", basicProperties, messageBytes);
+
+            // Configure return handler
+            EventingBasicConsumer rpcEventingBasicConsumer = new EventingBasicConsumer(channel);
+            rpcEventingBasicConsumer.Received += TwoWayMessageEventReceiver;
+
+            // Listen for reply
+            channel.BasicConsume(rpcResponseQueue, false, rpcEventingBasicConsumer);
+        }
+
+        private static void TwoWayMessageEventReceiver(object sender, BasicDeliverEventArgs e)
+        {
+            // Set up the IBasicProperties object and specify the temporary queue name to reply to and the correlation ID
+            IBasicProperties props = e.BasicProperties;
+            if (props != null && props.CorrelationId == correlationId)
+            {
+                string response = Encoding.UTF8.GetString(e.Body);
+                responseFromConsumer = response;
+            }
+
+            // Consume message
+            channel.BasicAck(e.DeliveryTag, false);
+            Console.WriteLine($"Response: {responseFromConsumer}");
+
+            // Prompt for reply
+            Console.WriteLine("Enter your message and press Enter.");
+            message = Console.ReadLine();
+
+            // Send Reply
+            messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", "mycompany.queues.rpc", props, messageBytes); //empty string parameter denotes the nameless default AMQP exchange
+        }
+    }
+}
